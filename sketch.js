72bdf49c168734079de760979bf50f55251d07d8
vaishnaviagarwal27 <61281973+vaishnaviagarwal27@users.noreply.github.com>
2020-08-18 15:44:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var canvas,database;
+var gameState=0;
+var playerCount;
+var form,game,player;
+var allPlayers;
+var car1,car2,car3,car4;
+var cars;
+
+function setup(){
+  canvas=createCanvas(displayWidth,displayHeight);
+  database=firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  if(playerCount===4){
+    game.update(1);
+  }
+  if(gameState===1){
+    clear();
+    game.play();
+  }
+
+
+
+}
